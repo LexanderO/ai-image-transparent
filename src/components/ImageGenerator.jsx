ea4e4390d650048c6d47946e66d4f78034deba7f
LexanderO <alexanderowens1993@gmail.com>
2023-04-10 18:02:50 +0100

    added loading bar

--- src/components/ImageGenerator.jsx
+++ src/components/ImageGenerator.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
+import LoadingBar from "react-top-loading-bar";
 
 function ImageGenerator() {
-  const [description, setDescription] = useState('');
-  const [imageUrl, setImageUrl] = useState('');
-  const [previewUrl, setPreviewUrl] = useState('');
+  const [description, setDescription] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [previewUrl, setPreviewUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [loadingMessage, setLoadingMessage] = useState("");
+  const [progress, setProgress] = useState(0);
 
   const ai_key = "Bearer " + import.meta.env.VITE_APP_OPENAI_API_KEY;
   const removebg_key = import.meta.env.VITE_APP_REMOVEBG_API_KEY;
@@ -15,39 +19,56 @@ function ImageGenerator() {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    setLoading(true);
+    setLoadingMessage("Generating image...");
     try {
       const openaiResponse = await axios.post(
-        'https://api.openai.com/v1/images/generations',
+        "https://api.openai.com/v1/images/generations",
         {
-          model: 'image-alpha-001',
-          prompt: `Generate an image of ${description}`,
-          response_format: 'url',
+          model: "image-alpha-001",
+          prompt: `${description}`,
+          response_format: "url",
         },
         {
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
             Authorization: ai_key,
           },
         }
       );
+      setLoadingMessage("Removing background...");
+      setProgress(50);
       const imageUrl = openaiResponse.data.data[0].url;
       const removeBgResponse = await axios.post(
-        'https://api.remove.bg/v1.0/removebg',
-        { image_url: imageUrl, size: 'regular' },
+        "https://api.remove.bg/v1.0/removebg",
+        { image_url: imageUrl, size: "regular" },
         {
           headers: {
-            'X-Api-Key': removebg_key,
+            "X-Api-Key": removebg_key,
           },
-          responseType: 'arraybuffer',
+          responseType: "arraybuffer",
+          onDownloadProgress: (progressEvent) => {
+            const percentCompleted = Math.round(
+              (progressEvent.loaded * 100) / progressEvent.total
+            );
+            setProgress(percentCompleted);
+          },
         }
       );
+      setLoadingMessage("Processing image...");
       const blob = new Blob([removeBgResponse.data], {
-        type: 'image/png',
+        type: "image/png",
       });
       const url = URL.createObjectURL(blob);
       setImageUrl(url);
+      setLoadingMessage("");
+      setProgress(100);
+      setLoading(false);
     } catch (error) {
       console.error(error);
+      setLoadingMessage("");
+      setProgress(0);
+      setLoading(false);
     }
   };
 
@@ -65,6 +86,8 @@ function ImageGenerator() {
       </form>
       {previewUrl && <img src={previewUrl} alt="Preview" />}
       {imageUrl && <img src={imageUrl} alt="Processed" />}
+      <LoadingBar progress={progress} height={5} color="#00a8ff" />
+      {loading && <div>{loadingMessage}</div>}
     </div>
   );
 }
