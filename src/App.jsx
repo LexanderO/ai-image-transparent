8ab51395fb391ecf4be0a173a31d8c16af2bb38c
LexanderO <alexanderowens1993@gmail.com>
2023-04-11 11:21:28 +0100

    added background

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ImageGenerator from './components/ImageGenerator';
 const App = () => {
   
   return (
-    <div>
+    <div className='bg-[#494D5F] h-screen' >
       <ImageGenerator />
     </div>
   )
