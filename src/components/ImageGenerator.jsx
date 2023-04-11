7e092ae77ae6f72c8568d9b89b262b0ff43649fa
LexanderO <alexanderowens1993@gmail.com>
2023-04-11 02:01:07 +0100

    styled page into responsive layout

--- src/components/ImageGenerator.jsx
+++ src/components/ImageGenerator.jsx
@@ -22,6 +22,7 @@ function ImageGenerator() {
     setImageUrl("");
     setPreviewUrl("");
     setLoading(true);
+    setProgress(10);
     setLoadingMessage("Generating image...");
     try {
       const openaiResponse = await axios.post(
@@ -75,42 +76,69 @@ function ImageGenerator() {
   };
 
   return (
-    <div className="max-w-md mx-auto">
-      <form onSubmit={handleFormSubmit} className="mb-4">
-        <label htmlFor="description" className="block mb-2 text-gray-700">
-          Enter an image description:
-        </label>
-        <input
-          type="text"
-          id="description"
-          value={description}
-          onChange={handleDescriptionChange}
-          className="w-full border border-gray-400 py-2 px-3 rounded-lg mb-4"
-        />
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Generate Image
-        </button>
-      </form>
-      {previewUrl && (
-        <img
-          src={previewUrl}
-          alt="Preview"
-          className="max-w-full mb-4 rounded-lg"
-        />
-      )}
-      {imageUrl && (
-        <img
-          src={imageUrl}
-          alt="Processed"
-          className="max-w-full mb-4 rounded-lg"
-        />
-      )}
+    <div className="">
+      <div className="flex flex-col sm:flex-row-reverse gap-10 m-5">
+        <div className="w-full md:w-1/2 min-w-30 relative top-[10px] sm:top-[200px]">
+          <form onSubmit={handleFormSubmit}>
+            <label
+              htmlFor="description"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Generate
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  aria-hidden="true"
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 20l4-2 14-14-2-2-14 14-2 4zM9.041 27.097c-0.989-2.085-2.052-3.149-4.137-4.137l3.097-8.525 4-2.435 12-12h-6l-12 12-6 20 20-6 12-12v-6l-12 12-2.435 4z"></path>
+                </svg>
+              </div>
+              <input
+                type="text"
+                id="description"
+                value={description}
+                onChange={handleDescriptionChange}
+                className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Enter an image description: ..."
+                required
+              />
+              <button
+                type="submit"
+                className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Generate
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="w-full md:flex-initial">
+        
+          {previewUrl && (
+            <img
+              src={previewUrl}
+              alt="Preview"
+              className="max-w-full mb-4 rounded-lg"
+            />
+          )}
+          {imageUrl && (
+            <img
+              src={imageUrl}
+              alt="Processed"
+              className="max-w-full mb-4 rounded-lg"
+            />
+          )}
+        </div>
+      </div>
+
       <LoadingBar
         progress={progress}
-        height={5}
+        height={10}
         color="#00a8ff"
         className="mb-4"
       />
