0ba753e5714c1e586de462679544daf48d63928b
LexanderO <alexanderowens1993@gmail.com>
2023-04-12 21:49:24 +0100

    fixed footer and renamed title

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <h1 className="sm:text-[10px] text-[8px] font-black text-white text-center">Made by LexanderO</h1>
+      <h1 className="sm:text-[10px] text-[8px] font-black text-white text-center">©2023 LexanderO</h1>
     </div>
   );
 };
